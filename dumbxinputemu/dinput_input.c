354f5193aaa4c1fea9ae6d8538e4038e6fb0e8ef
Sanjay Govind <sanjay.govind9@gmail.com>
2025-12-11 12:44:45 +1300

    gh3 does not like rb guitars, so make every guitar a gh one

--- dumbxinputemu/dinput_input.c
+++ dumbxinputemu/dinput_input.c
@@ -119,7 +119,7 @@ static BOOL dinput_is_good(const LPDIRECTINPUTDEVICE8A device, struct CapsFlags 
     if (property.dwData == MAKELONG(0x1BAD, 0x4734) || property.dwData == MAKELONG(0x0738, 0x9806) || property.dwData == MAKELONG(0x1BAD, 0x0719)) {
         TRACE("Setting subtype to guitar!\n");
         TRACE("XInput guitar detected!\n");
-        caps->subtype = XINPUT_DEVSUBTYPE_GUITAR;
+        caps->subtype = XINPUT_DEVSUBTYPE_GUITAR_ALTERNATE;
     }
     if (property.dwData == MAKELONG(0x1BAD, 0x0130)) {
         TRACE("Setting subtype to drumd!\n");
